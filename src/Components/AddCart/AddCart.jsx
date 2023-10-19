d1edfcb457b6566cbb6ed9acf250b79bce01e224
Mehedi Hassan <mh93075@gmail.com>
2023-10-19 16:10:01 +0600

    My Cart Section done without design

--- src/Components/AddCart/AddCart.jsx
+++ src/Components/AddCart/AddCart.jsx
@@ -2,10 +2,21 @@ import { useLoaderData } from "react-router-dom";
 
 const AddCart = () => {
     const cartProducts = useLoaderData();
+    console.log(cartProducts)
 
     return (
         <div>
             <h1>Add Cart: {cartProducts.length}</h1>
+            <div>
+                {
+                    cartProducts.map((mycart, index) => (
+                        <div key={mycart._id}>
+                            <h1>No. {index + 1}</h1>
+                            <h1>Name: {mycart.name}</h1>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     );
 };
