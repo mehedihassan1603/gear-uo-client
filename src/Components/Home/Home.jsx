0588b79a98198dd4f3a0b5fac14b435b05a9e610
Mehedi Hassan <mh93075@gmail.com>
2023-10-21 23:55:18 +0600

    Dark Mode active on Homepage

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,8 @@
+import React, { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import Banner from "../Banner/Banner";
 import Footer from "../Footer/Footer";
 import Items from "../Items/Items";
-import { useEffect } from "react";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import "./Home.css";
@@ -12,7 +12,6 @@ import ServicesComponent from "./ServicesComponent";
 
 const Home = () => {
   const products = useLoaderData();
-  console.log(products);
 
   useEffect(() => {
     Aos.init({
@@ -20,31 +19,44 @@ const Home = () => {
     });
   }, []);
 
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme === "dark") {
+      setIsDarkMode(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("theme", isDarkMode ? "dark" : "light");
+  }, [isDarkMode]);
+
   return (
-    <div>
-      <Banner></Banner>
-      <TopBrand></TopBrand>
+    <div className={`theme-${isDarkMode ? "dark" : "light"}`}>
+      <button
+        className="theme-toggle-button"
+        onClick={() => setIsDarkMode(!isDarkMode)}
+      >
+        {isDarkMode ? "Light Mode" : "Dark Mode"}
+      </button>
+      <Banner />
+      <TopBrand />
+
       <div className="w-full mx-auto rounded-lg p-4 mb-10">
         <h1 className="marquee text-5xl font-avenir font-semibold py-3 text-slate-100 text-center">
           <span className=" bg-slate-800 p-3 rounded-2xl">Choose a Brand that you want to explore!!!</span>
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {products.map((data, index) => (
-            <div
-              key={data.id}
-              data-aos={"slide-up"}
-              data-aos-delay="100"
-            >
+            <div key={data.id} data-aos={"slide-up"} data-aos-delay="100">
               <Items product={data}></Items>
             </div>
           ))}
         </div>
       </div>
 
-
-      <ServicesComponent></ServicesComponent>
-
-
+      <ServicesComponent />
 
       <div className="flex flex-col md:flex-col lg:flex-row justify-center items-center px-20 gap-10 py-10 bg-slate-800">
         <div className="hero bg-sky-200 rounded-lg shadow-lg card-hover">
@@ -73,11 +85,8 @@ const Home = () => {
         </div>
       </div>
 
-
-      <ObservationForm></ObservationForm>
-
-
-      <Footer></Footer>
+      <ObservationForm />
+      <Footer />
     </div>
   );
 };
